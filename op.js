1f207fdec0c49be8394cdccaccf910e666454568
SherleyHoover <3030756558@qq.com>
2026-03-26 19:21:24 +0800

    Add files via upload

---
+++ op.js
@@ -0,0 +1,12 @@
+console.log("Licensed under the Jamesonian Public License v1.0. See LICENSE for details.");
+window.alert("For your safety, all your subsequent actions on this website will be monitored and recorded by the Bigtu Ministry of Security. Do you wish to proceed?");
+document.getElementById("MyHead").textContent = `Welcome to the first webpage of Jamesonia!`;
+document.getElementById("MyPara").textContent= `Launched on 1979-12-31, New Carthage, Jamesonian State Instruments`;
+
+
+
+
+
+
+
+
