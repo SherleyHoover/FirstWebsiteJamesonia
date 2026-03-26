c0552880c60101402a33fa721317695f7b60a89d
SherleyHoover <3030756558@qq.com>
2026-03-26 19:27:34 +0800

    Update op.js

--- op.js
+++ op.js
@@ -1,5 +1,6 @@
 console.log("Licensed under the Jamesonian Public License v1.0. See LICENSE for details.");
-window.alert("For your safety, all your subsequent actions on this website will be monitored and recorded by the Bigtu Ministry of Security. Do you wish to proceed?");
+window.alert("You are entering this foreign website as a Countrian citizen.");
+window.alert("For your safety, all your subsequent actions on this website will be monitored and recorded by the Countrian National Intelligence Bureau. Do you wish to proceed?");
 document.getElementById("MyHead").textContent = `Welcome to the first webpage of Jamesonia!`;
 document.getElementById("MyPara").textContent= `Launched on 1979-12-31, New Carthage, Jamesonian State Instruments`;
 
